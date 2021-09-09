7513f6f719e7f22b7cd7117dbfa19a55f6253caf
DvegaDv01 <ben@Invader-Vim.localdomain>
2021-09-08 20:48:16 -0400

    adding python file

---
+++ python/HelloVariableWorld.py
@@ -0,0 +1,34 @@
+# Create a variable called 'name' that holds a string
+
+name = 'Blackjack Foley'
+# Create a variable called 'country' that holds a string
+country = 'US'
+
+# Create a variable called 'age' that holds an integer
+
+age = 28
+# Create a variable called 'hourly_wage' that holds an integer
+hourly_wage = 60
+
+# Calculate the daily wage for the user
+daily_working_hrs = 8
+
+daily_wage = daily_working_hrs * hourly_wage
+
+# Create a variable called 'satisfied' that holds a boolean
+
+satisfied = True
+# Print out "Hello <name>!"
+print(f'Hello {name}')
+
+# Print out what country the user entered
+print(f'{country}')
+
+# Print out the user's age
+print(f'{age}')
+
+# With an f-string, print out the daily wage that was calculated
+print(f'${daily_wage}')
+
+# With an f-string, print out whether the users were satisfied
+print(f'{satisfied}')
